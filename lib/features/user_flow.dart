d8e9a431cf5d8ac47475b44ebd71b9e6b98ac515
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-10-21 21:39:20 -0500

    updated the server IP

--- lib/features/user_flow.dart
+++ lib/features/user_flow.dart
@@ -24,7 +24,7 @@ class _UserFlowPageState extends State<userFlowPage> {
 
       // Send the file to the FastAPI endpoint
       var request = http.MultipartRequest(
-          'POST', Uri.parse('http://localhost:8801/upload-csv'));
+          'POST', Uri.parse('http://45.55.39.250/upload-csv'));
       request.files.add(http.MultipartFile(
           'file', file.readAsBytes().asStream(), file.lengthSync(),
           filename: file.path.split("/").last));
